05937c449c2f79ecc2cf4a3d477488ecdc628f10
Abdullokh Musaev <abdulloh.m@payme.uz>
2024-07-27 16:09:01 +0500

    cdk stack use dotenv

--- cdk_deployment/cart-service.go
+++ cdk_deployment/cart-service.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
+	"github.com/joho/godotenv"
 )
 
 type CartServiceStackProps struct {
 	awscdk.StackProps
+	dbHost     string
+	dbPort     string
+	dbUsername string
+	dbPassword string
+	dbName     string
 }
 
 func NewCartServiceStack(scope constructs.Construct, id string, props *CartServiceStackProps) awscdk.Stack {
@@ -25,6 +34,13 @@ func NewCartServiceStack(scope constructs.Construct, id string, props *CartServi
 		Code:         awslambda.Code_FromAsset(jsii.String("../dist"), nil),
 		Runtime:      awslambda.Runtime_NODEJS_18_X(),
 		Handler:      jsii.String("main.handler"),
+		Environment: &map[string]*string{
+			"DB_HOST":     jsii.String(props.dbHost),
+			"DB_PORT":     jsii.String(props.dbPort),
+			"DB_USERNAME": jsii.String(props.dbUsername),
+			"DB_PASSWORD": jsii.String(props.dbPassword),
+			"DB_NAME":     jsii.String(props.dbName),
+		},
 	})
 
 	// * apigateway instance
@@ -42,11 +58,29 @@ func NewCartServiceStack(scope constructs.Construct, id string, props *CartServi
 }
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	defer jsii.Close()
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 
 	app := awscdk.NewApp(nil)
 
-	NewCartServiceStack(app, "CartServiceStack", &CartServiceStackProps{})
+	NewCartServiceStack(app, "CartServiceStack", &CartServiceStackProps{
+		dbHost:     dbHost,
+		dbPort:     dbPort,
+		dbUsername: dbUsername,
+		dbPassword: dbPassword,
+		dbName:     dbName,
+	})
 
 	app.Synth(nil)
 }
